202af5fc4a9055e460f90e925718944b7a10cb49
broBbang2 <msk990719@gmail.com>
2025-06-06 21:19:35 +0900

    2일차: 경기 슬라이드 UI 구현 완료

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ const menuBtn = document.getElementById('menuToggleBtn');
 const mainNav = document.querySelector('.main-nav');
 const rightNav = document.querySelector('.right-nav');
 
-menuBtn.addEventListener('click', () {
+menuBtn.addEventListener('click', function () {
   mainNav.classList.toggle('show');
   rightNav.classList.toggle('show');
+});
+
+const swiper = new Swiper(".match-swiper", {
+  loop: false,
+  autoplay: false,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+},
+  slidesPerView: 3,
+  spaceBetween: 24,
 });
